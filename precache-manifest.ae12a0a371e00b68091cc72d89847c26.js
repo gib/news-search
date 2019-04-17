c93438b5fc1eaaf8f387a112ee76d7e6f1d14f8e
gib <gib@users.noreply.github.com>
2019-04-17 09:54:29 -0600

    prod build update

---
+++ precache-manifest.ae12a0a371e00b68091cc72d89847c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff17bdf0610b2ed444d",
+    "url": "/news-search/static/css/main.342c731e.chunk.css"
+  },
+  {
+    "revision": "7ff17bdf0610b2ed444d",
+    "url": "/news-search/static/js/main.39d3479d.chunk.js"
+  },
+  {
+    "revision": "eecd179f1bce877df4ad",
+    "url": "/news-search/static/js/runtime~main.85bf6d6c.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/news-search/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "e7469b5bd3f6c0c6a74ab99d6274f73d",
+    "url": "/news-search/index.html"
+  }
+];
